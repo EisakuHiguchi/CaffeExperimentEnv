73aa09b993aa67a9dcf6217fe89d58b7b0b67ccf
EisakuHiguchi <13ec083@ms.dendai.ac.jp>
2016-03-03 18:40:57 +0900

    ubuntu用に調整して動作まで確認 細かい機能追加は後々?

--- CaffeExperimentEnv/CaffeExperimentEnv/CaffeExperimentEnv.py
+++ CaffeExperimentEnv/CaffeExperimentEnv/CaffeExperimentEnv.py
@@ -1,14 +1,90 @@
+#coding:utf-8
+
+import re
 import wx
 import subprocess as s
 import os
 import datetime
 from stat import *
+import codecs
+from collections import OrderedDict
 
 executeName = u""
 dir_path = u""
 
+prmdict = OrderedDict((
+    (u"test_iter",u"100"),
+    (u"test_interval",u"500"),
+    (u"base_lr",u"0.01"),
+    (u"momentum",u"0.9"),
+    (u"weight_decay",u"0.004"),
+    (u"lr_policy",u"fixed"),
+   # (u"gamma",u"0.0001"),
+   # (u"power",u"0.75"),
+    (u"display",u"100"),
+    (u"max_iter",u"4000"),
+    (u"snapshot",u"4000"),
+    (u"snapshot_format",u"HDF5"),
+    (u"snapshot_prefix",u"dir_path"),
+    (u"solver_mode",u"GPU")
+    ))
+
+db_dict = { 
+    u"height":"128", 
+    u"width":"128"
+    }
+
+
 set_dir_border = 3
 panel_border = 5
+
+
+def replace(path, search, replace):
+    read_file = ""
+    write_file = ""
+    temp_file = u"temp_file"
+    search_t = search.encode('utf-8')
+    replace_t = replace.encode('utf-8')
+    try:
+        read_file = open(path, 'r')
+        write_file = open(temp_file, 'w')
+        for line in read_file:
+            line_t = line
+            if line_t.find(search_t) != -1:
+                line_t = re.sub(search_t, replace_t, line)
+            write_file.write(line_t)
+    finally:
+        read_file.close()
+        write_file.close()
+
+    if os.path.isfile(path) and os.path.isfile(temp_file):
+        os.remove(path)
+        os.rename(temp_file, path)
+    return 0
+
+
+def editConfig():
+    data = u"net: " + "\"" + os.path.join(dir_path, "quick_train_test.prototxt") + "\""
+    for k, v in prmdict.items():
+        if k == "lr_policy":
+            data = data + u"\n" + k + u": \"" + v + "\""
+        elif k == "snapshot_prefix":
+            data = data + u"\n" + k + u": \"" + dir_path + "\""
+        else:
+            data = data + u"\n" + k + u": " + v
+    f = open(dir_path + "/quick_solver.prototxt", "w")
+    f.write(data.encode('utf-8'))
+    f.close()
+
+    replace(dir_path + "/quick.prototxt", u"dim: w", u"dim: " + db_dict["width"])
+    replace(dir_path + "/quick.prototxt", u"dim: h", u"dim: " + db_dict["height"])
+    replace(dir_path + "/quick_train_test.prototxt", u"mean.binaryproto", os.path.join(dir_path, u"mean.binaryproto"))
+    replace(dir_path + "/quick_train_test.prototxt"
+            , u"train_leveldb"
+            , os.path.join(dir_path, u"train_leveldb"))
+    replace(dir_path + "/quick_train_test.prototxt"
+            , u"test_leveldb"
+            , os.path.join(dir_path, u"test_leveldb"))
 
 def startTime():
     global executeName
@@ -17,12 +93,18 @@ def startTime():
     return executeName
 
 def createConfig(path):
-    s.call("copy "
-        + r"config\config_test.txt" # \ > /
-        + " " + os.path.join(path, "config_test.txt")
+    s.call("cp "
+        + r"config/quick.prototxt" # \ > /
+        + " " + os.path.join(path, "quick.prototxt")
         , shell = True # for win
-        ) # \ > /
+        )
+    s.call("cp "
+        + r"config/quick_train_test.prototxt" # \ > /
+        + " " + os.path.join(path, "quick_train_test.prototxt")
+        , shell = True # for win
+        )
     # edit config
+    editConfig()
 
 
 def createDir(dir):
@@ -38,64 +120,79 @@ def createDir(dir):
 
 def createDB(type):
     if type == "LEVELDB" or type == "LMDB":
-        s.call(caffe_Panels[1].Value + "\\" + "convert_imageset "  # \\
-               + dir_path + " " # image root
+        s.call(caffe_Panels[1].Value + "/" + "convert_imageset "  # \\
+               + image_Panels[1].Value + "/ " # image root
                + trainlist_Panels[1].Value + " " # train list
-               + executeName + "_train" # 
+               + dir_path + "/" + "train_leveldb"
                + " -shuffle "
                + " -backend leveldb" 
                + " -resize_height " + resize_Panels[0].Value
                + " -resize_width " + resize_Panels[1].Value
+               , shell = True
                ) # wait process end ??
-        s.call(caffe_Panels[1].Value + "\\" + "convert_imageset "  # \\
-               + dir_path + " " # image root
-               + test_list_path + " " # test list
-               + testlist_Panels[1].Value + "_test" # 
+        s.call(caffe_Panels[1].Value + "/" + "convert_imageset "  # \\
+               + image_Panels[1].Value + "/ " # image root
+               + testlist_Panels[1].Value + " " # test list
+               + dir_path + "/" + "test_leveldb" 
                + " -shuffle "
                + " -backend leveldb" 
                + " -resize_height " + resize_Panels[0].Value
                + " -resize_width " + resize_Panels[1].Value
-               )
+               , shell = True
+               ) # wait process end ??
     else:
         wx.MessageBox("type: LEVELDB or LMDB")
 
 
 def createMean():
-    s.call(caffe_Panels[1].Value + "\\" + "compute_image_mean " # \\
-           + dir_path + " " # db root
-           + dir_path + "\\" + "mean.binarypoto" # \\ mean path
-           + " -backend leveldb")
+    print(caffe_Panels[1].Value + "/" + "compute_image_mean" # \\
+           + " -backend leveldb "
+           + dir_path + "/" + "train_leveldb " # db root
+           + dir_path + "/" + "mean.binaryproto" # \\ mean path
+           )
+    s.call(caffe_Panels[1].Value + "/" + "compute_image_mean" # \\
+           + " -backend leveldb "
+           + dir_path + "/" + "train_leveldb " # db root
+           + dir_path + "/" + "mean.binaryproto" # \\ mean path
+           , shell = True
+           )
 
 
 def trainModel():
-    s.call(caffe_Panels[1].Value + "\\" + "caffe" # \\
+    s.call(caffe_Panels[1].Value + "/" + "caffe" # \\
            + " train -solver "
-           + dir_path + "\\quick_solver.prototxt") # solver path
-        
-
+           + dir_path + "/quick_solver.prototxt"
+           , shell = True
+           ) # solver path
 
 def click_button_1(event):
-    cmd = listbox.GetSelection
-    if cmd == "createNewDir":
+    global dir_path
+    dir_path = dirPath_Panels[1].Value
+    cmd = listbox.GetSelection()
+    if cmd == 0: # createNewDir
         createDir(startTime())
-    elif cmd == "createDataBase":
-        createDB()
-    elif cmd == "createMean":
+    elif cmd == 1: # createDataBase
+        createDB("LEVELDB")
+    elif cmd == 2: # createMean
         createMean()
-    elif cmd == "trainModel":
+    elif cmd == 3: # trainModel
         trainModel()
-    else:
+    else: # all
         createDir(startTime())
-        createDB()
+        createDB("LEVELDB")
         createMean()
         trainModel()
 
 
 def click_openDialog(event):
     result = ""
-    # dialog
-    dlg = wx.DirDialog(None,"Choose input directory","",
+    # dir dialog
+    if event.GetId() < 4:
+        dlg = wx.DirDialog(None,"Choose directory","",
                        wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST)
+    else:
+        dlg = wx.FileDialog(None,"Choose file","", "","*.*")
+
     if dlg.ShowModal() == wx.ID_OK:
         result = dlg.GetPath() # get fullpath
     dlg.Destroy()
@@ -107,7 +204,12 @@ def click_openDialog(event):
         image_Panels[1].Value = result
     elif id == 2: # caffe
         caffe_Panels[1].Value = result
-
+    elif id == 4: # trainlist
+        trainlist_Panels[1].Value = result
+    elif id == 5: # testlist
+        testlist_Panels[1].Value = result
+    elif id == 3: # dirPath
+        dirPath_Panels[1].Value = result
 
 def getDirPanel(panel, text, textbox, id):
     new_panel = wx.Panel(panel, wx.ID_ANY)
@@ -173,33 +275,17 @@ if __name__ == "__main__":
     # setting path
     home_Panels = getDirPanel(panel, u"home", u"home dir", 0)
     image_Panels = getDirPanel(panel, u"image", u"image dir", 1)
-    trainlist_Panels = getDirPanel(panel, u"trainlist", u"trainlist path", 3)
-    testlist_Panels = getDirPanel(panel, u"testlist", u"testlist path", 4)
+    trainlist_Panels = getDirPanel(panel, u"trainlist", u"trainlist path", 4)
+    testlist_Panels = getDirPanel(panel, u"testlist", u"testlist path", 5)
+    dirPath_Panels = getDirPanel(panel, u"dirpath", u"default auto", 3)
     caffe_Panels = getDirPanel(panel, u"caffe", u"~/caffe/build/tools", 2)
 
     # setting resize
     resize_panel = wx.Panel(sub_panel, wx.ID_ANY)
     resize_layout = wx.BoxSizer(wx.VERTICAL)
-    resize_Panels = getPrmPanels(
-        { u"height":"128", u"width":"128"}, 
-        panel, resize_panel, resize_layout)
+    resize_Panels = getPrmPanels(db_dict, panel, resize_panel, resize_layout)
 
     # setting prm
-    prmdict = {
-        u"test_iter":"100",
-        u"test_interval":"500",
-        u"base_lr":"0.01",
-        u"momentum":"0.9",
-        u"weight_decay":"0.005",
-        u"lr_policy":u"inv",
-        u"gamma":"0.0001",
-        u"power":"0.75",
-        u"display":"100",
-        u"max_iter":"10000",
-        u"snapshot":"5000",
-        u"snapshot_prefix":u"dir_path",
-        u"solver_mode":u"GPU"
-        }
     prm_panel = wx.Panel(sub_panel, wx.ID_ANY)
     prm_layout = wx.BoxSizer(wx.VERTICAL)
     prm_Panels = getPrmPanels(prmdict, panel, prm_panel, prm_layout)
@@ -210,7 +296,7 @@ if __name__ == "__main__":
     exec_layout = wx.BoxSizer(wx.HORIZONTAL)
     
     element_array = (
-        "createNewDir"
+        "createNewDir",
         "createDataBase", 
         "createMean", 
         "trainModel",
@@ -231,6 +317,7 @@ if __name__ == "__main__":
     layout.Add(image_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
     layout.Add(trainlist_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
     layout.Add(testlist_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
+    layout.Add(dirPath_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
     layout.Add(caffe_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
     sub_layout.Add(prm_panel, flag = wx.EXPAND | wx.ALL, border = panel_border)
     sub_layout.Add(resize_panel, flag = wx.EXPAND | wx.ALL, border = panel_border)

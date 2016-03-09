844bd5258a000cd74f8d02bb1e2268b03a167c22
EisakuHiguchi <13ec083@ms.dendai.ac.jp>
2016-03-09 23:08:41 +0900

    リファクタ

--- CaffeExperimentEnv/CaffeExperimentEnv/CaffeExperimentEnv.py
+++ CaffeExperimentEnv/CaffeExperimentEnv/CaffeExperimentEnv.py
@@ -19,8 +19,6 @@ prmdict = OrderedDict((
     (u"momentum",u"0.9"),
     (u"weight_decay",u"0.004"),
     (u"lr_policy",u"fixed"),
-   # (u"gamma",u"0.0001"),
-   # (u"power",u"0.75"),
     (u"display",u"100"),
     (u"max_iter",u"4000"),
     (u"snapshot",u"4000"),
@@ -34,9 +32,92 @@ db_dict = {
     u"width":"128"
     }
 
+path_dict = OrderedDict((
+    (u"home", [u"home dir", 0]),
+    (u"image", [u"image dir", 1]),
+    (u"trainlist", [u"trainlist path", 4]),
+    (u"testlist", [u"testlist path", 5]),
+    (u"dirpath", [u"default auto", 3]),
+    (u"caffe", [u"~/caffe/build/tools", 2])
+    ))
+
 
 set_dir_border = 3
 panel_border = 5
+
+class prmGroup:    
+    def __init__(self, dict, basepanel):
+        self.value = dict
+        self.base = basepanel
+        self.panel = wx.Panel(basepanel, wx.ID_ANY)
+        self.layout = wx.BoxSizer(wx.VERTICAL)
+        # create
+        self.controls = self.getPrmPanels()
+        self.panel.SetSizer(self.layout)
+
+    def getValue(self, key):
+        temp = self.controls[key]
+        return temp.value
+
+    def refresh(self):
+        self.panel.SetSizer(self.layout)
+        
+    def getPrmPanel(self,text, textbox):
+        new_panel = wx.Panel(self.panel, wx.ID_ANY)
+        np_layout = wx.BoxSizer(wx.HORIZONTAL)
+        new_text = wx.StaticText(new_panel, wx.ID_ANY, text)
+        new_textbox = wx.TextCtrl(new_panel, wx.ID_ANY, textbox, size=(100,20))
+        np_layout.Add(new_text, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
+        np_layout.Add(new_textbox, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
+        new_panel.SetSizer(np_layout)
+        return (new_panel, new_textbox)
+
+    def getPrmPanels(self):
+        panels = {}
+        for k, v in self.value.items():
+            temp = self.getPrmPanel(k, v)
+            panels[k] = temp[1]
+            self.layout.Add(temp[0])
+        return panels
+
+class DirGroup:
+    def __init__(self, dict, basepanel, layout):
+        self.value = dict
+        self.base = basepanel
+        self.layout = layout
+        self.controls = self.getDirPanels()
+
+    def getValue(self, key):
+        temp = self.controls[key]
+        return temp.value
+
+    def setValue(self, key, val):
+        temp = self.controls[key]
+        temp.value = val
+
+    def refresh(self):
+        self.panel.SetSizer(self.layout)
+
+    def getDirPanel(self, text, textbox, id):
+        new_panel = wx.Panel(self.base, wx.ID_ANY)
+        np_layout = wx.BoxSizer(wx.HORIZONTAL)
+        new_dir_text = wx.StaticText(new_panel, wx.ID_ANY, text)
+        new_dir_textbox = wx.TextCtrl(new_panel, wx.ID_ANY, textbox, size=(500,20))
+        new_dir_button = wx.Button(new_panel, id, u"ref", size=(50,20))
+        new_dir_button.Bind(wx.EVT_BUTTON, click_openDialog)
+        np_layout.Add(new_dir_text, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
+        np_layout.Add(new_dir_button, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
+        np_layout.Add(new_dir_textbox, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
+        new_panel.SetSizer(np_layout)
+        return (new_panel, new_dir_textbox)
+
+    def getDirPanels(self):
+        panels = {}
+        for k, v in self.value.items():
+            temp = self.getDirPanel(k, v[0], v[1])
+            panels[k] = temp[1]
+            self.layout.Add(temp[0])
+        return panels
 
 
 def replace(path, search, replace):
@@ -67,11 +148,18 @@ def editConfig():
     data = u"net: " + "\"" + os.path.join(dir_path, "quick_train_test.prototxt") + "\""
     for k, v in prmdict.items():
         if k == "lr_policy":
-            data = data + u"\n" + k + u": \"" + v + "\""
+            data = u"%s\n" %data \
+                + u"%s: \"" %k \
+                + u"%s\"" %v
         elif k == "snapshot_prefix":
-            data = data + u"\n" + k + u": \"" + dir_path + "\""
+            data = u"%s\n" %data \
+                + u"%s: \"" %k \
+                + u"%s/quick\"" %dir_path
         else:
-            data = data + u"\n" + k + u": " + v
+            data = u"%s\n" %data \
+                + u"%s: " %k \
+                + u"%s" %v
+
     f = open(dir_path + "/quick_solver.prototxt", "w")
     f.write(data.encode('utf-8'))
     f.close()
@@ -93,16 +181,12 @@ def startTime():
     return executeName
 
 def createConfig(path):
-    s.call("cp "
-        + r"config/quick.prototxt" # \ > /
-        + " " + os.path.join(path, "quick.prototxt")
-        , shell = True # for win
-        )
-    s.call("cp "
-        + r"config/quick_train_test.prototxt" # \ > /
-        + " " + os.path.join(path, "quick_train_test.prototxt")
-        , shell = True # for win
-        )
+    p1 = "cp " + r"config/quick.prototxt" + " " + os.path.join(path, "quick.prototxt")
+    print(p1)
+    s.call(p1, shell = True)
+    p2 = "cp " + r"config/quick_train_test.prototxt" + " " + os.path.join(path, "quick_train_test.prototxt")
+    print(p2)
+    s.call(p2, shell = True)
     # edit config
     editConfig()
 
@@ -110,9 +194,8 @@ def createConfig(path):
 def createDir(dir):
     global dir_path
     dir_path = os.path.join(home_Panels[1].Value, dir)
-    s.call("mkdir " + dir_path
-           , shell=True # for win
-           ) # exeption > if exit
+    p = "mkdir " + dir_path
+    s.call(p, shell=True) # exeption > if exit
     home_Panels[1].Value = dir_path
     
     createConfig(dir_path)
@@ -120,50 +203,45 @@ def createDir(dir):
 
 def createDB(type):
     if type == "LEVELDB" or type == "LMDB":
-        s.call(caffe_Panels[1].Value + "/" + "convert_imageset "  # \\
-               + image_Panels[1].Value + "/ " # image root
-               + trainlist_Panels[1].Value + " " # train list
-               + dir_path + "/" + "train_leveldb"
-               + " -shuffle "
-               + " -backend leveldb" 
-               + " -resize_height " + resize_Panels[0].Value
-               + " -resize_width " + resize_Panels[1].Value
-               , shell = True
-               ) # wait process end ??
-        s.call(caffe_Panels[1].Value + "/" + "convert_imageset "  # \\
-               + image_Panels[1].Value + "/ " # image root
-               + testlist_Panels[1].Value + " " # test list
-               + dir_path + "/" + "test_leveldb" 
-               + " -shuffle "
-               + " -backend leveldb" 
-               + " -resize_height " + resize_Panels[0].Value
-               + " -resize_width " + resize_Panels[1].Value
-               , shell = True
-               ) # wait process end ??
+        p1 = "%s/convert_imageset" %caffe_Panels[1].Value \
+               + " -shuffle" \
+               + " -backend leveldb" \
+               + " -resize_height %s" %resize_Panels[0].Value \
+               + " -resize_width %s" %resize_Panels[1].Value \
+               + " %s/" %image_Panels[1].Value \
+               + " %s" %trainlist_Panels[1].Value \
+               + " %s/train_leveldb" %dir_path
+        print(p1)
+        s.call(p1, shell = True)
+        p2 = "%s/convert_imageset" %caffe_Panels[1].Value \
+               + " -shuffle" \
+               + " -backend leveldb" \
+               + " -resize_height %s" %resize_Panels[0].Value \
+               + " -resize_width %s" %resize_Panels[1].Value \
+               + " %s/" %image_Panels[1].Value \
+               + " %s" %testlist_Panels[1].Value \
+               + " %s/test_leveldb" %dir_path
+        print(p2)
+        s.call(p2, shell = True)
     else:
         wx.MessageBox("type: LEVELDB or LMDB")
 
 
 def createMean():
-    print(caffe_Panels[1].Value + "/" + "compute_image_mean" # \\
-           + " -backend leveldb "
-           + dir_path + "/" + "train_leveldb " # db root
-           + dir_path + "/" + "mean.binaryproto" # \\ mean path
-           )
-    s.call(caffe_Panels[1].Value + "/" + "compute_image_mean" # \\
-           + " -backend leveldb "
-           + dir_path + "/" + "train_leveldb " # db root
-           + dir_path + "/" + "mean.binaryproto" # \\ mean path
-           , shell = True
-           )
+    p = "%s/compute_image_mean" %caffe_Panels[1].Value \
+           + " -backend leveldb" \
+           + " %s/train_leveldb" %dir_path \
+           + " %s/mean.binaryproto" %dir_path # \\ mean path
+    print(p)
+    s.call(p, shell = True)
 
 
 def trainModel():
-    s.call(caffe_Panels[1].Value + "/" + "caffe" # \\
-           + " train -solver "
-           + dir_path + "/quick_solver.prototxt"
-           , shell = True
-           ) # solver path
+    p = "%s/caffe" %caffe_Panels[1].Value \
+           + " train -solver" \
+           + " %s/quick_solver.prototxt" %dir_path
+    print(p)
+    s.call(p, shell = True) # solver path
 
 def click_button_1(event):
     global dir_path
@@ -198,73 +276,30 @@ def click_openDialog(event):
     dlg.Destroy()
 
     id = event.GetId()
+
     if id == 0: # home
-        home_Panels[1].Value = result
+        dir_Controls.setValue("home", result)
     elif id == 1: # image
-        image_Panels[1].Value = result
+        dir_Controls.setValue("image", result)
     elif id == 2: # caffe
-        caffe_Panels[1].Value = result
+        dir_Controls.setValue("dirpath", result)
     elif id == 4: # trainlist
-        trainlist_Panels[1].Value = result
+        dir_Controls.setValue("trainlist", result)
     elif id == 5: # testlist
-        testlist_Panels[1].Value = result
+        dir_Controls.setValue("testlist", result)
     elif id == 3: # dirPath
-        dirPath_Panels[1].Value = result
-
-def getDirPanel(panel, text, textbox, id):
-    new_panel = wx.Panel(panel, wx.ID_ANY)
-    np_layout = wx.BoxSizer(wx.HORIZONTAL)
-    
-    new_dir_text = wx.StaticText(new_panel, wx.ID_ANY, text)
-    new_dir_textbox = wx.TextCtrl(new_panel, wx.ID_ANY, textbox
-                                   , size=(500,20))
-    new_dir_button = wx.Button(new_panel, id, u"ref", size=(50,20))
-    new_dir_button.Bind(wx.EVT_BUTTON, click_openDialog)
-    
-    np_layout.Add(new_dir_text, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
-    np_layout.Add(new_dir_button, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
-    np_layout.Add(new_dir_textbox, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
-
-    new_panel.SetSizer(np_layout)
-
-    return (new_panel, new_dir_textbox)
+        dir_Controls.setValue("dirpath", result)
 
 
-def getPrmPanel(panel, text, textbox):
-    new_panel = wx.Panel(panel, wx.ID_ANY)
-    np_layout = wx.BoxSizer(wx.HORIZONTAL)
-
-    new_text = wx.StaticText(new_panel, wx.ID_ANY, text)
-    new_textbox = wx.TextCtrl(new_panel, wx.ID_ANY, textbox
-                              , size=(100,20))
-
-    np_layout.Add(new_text, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
-    np_layout.Add(new_textbox, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
-
-    new_panel.SetSizer(np_layout)
-    return (new_panel, new_textbox)
-
-
-def getPrmPanels(prmdict, panel, prm_panel, prm_layout):
-    panels = []
-    for k, v in prmdict.items():
-        temp = getPrmPanel(prm_panel, k, v)
-        panels.append(temp[1])
-        prm_layout.Add(temp[0])
-    prm_panel.SetSizer(prm_layout)
-    return panels
 
 
 
 if __name__ == "__main__":
     application = wx.App()
-    frame = wx.Frame(None, wx.ID_ANY, u"Test", size=(600,650))
-    
-    # main panel
-    panel = wx.Panel(frame, wx.ID_ANY)
-    #panel.SetBackgroundColour("#FFFFFFF")
-    layout = wx.BoxSizer(wx.VERTICAL)
+    frame = wx.Frame(None, wx.ID_ANY, u"Caffeteria", size=(600,650))
 
+    panel = wx.Panel(frame, wx.ID_ANY)
+    layout = wx.BoxSizer(wx.VERTICAL)
     sub_panel = wx.Panel(panel, wx.ID_ANY)
     sub_layout = wx.BoxSizer(wx.HORIZONTAL)
 
@@ -272,60 +307,36 @@ if __name__ == "__main__":
     button_1 = wx.Button(panel, wx.ID_ANY, u"Go!", size=(50,50))
     button_1.Bind(wx.EVT_BUTTON, click_button_1)
 
-    # setting path
-    home_Panels = getDirPanel(panel, u"home", u"home dir", 0)
-    image_Panels = getDirPanel(panel, u"image", u"image dir", 1)
-    trainlist_Panels = getDirPanel(panel, u"trainlist", u"trainlist path", 4)
-    testlist_Panels = getDirPanel(panel, u"testlist", u"testlist path", 5)
-    dirPath_Panels = getDirPanel(panel, u"dirpath", u"default auto", 3)
-    caffe_Panels = getDirPanel(panel, u"caffe", u"~/caffe/build/tools", 2)
+    # controls
+    dir_Controls = DirGroup(path_dict,panel,layout)
+    resize_Controls = prmGroup(db_dict, sub_panel)
+    prm_Controls = prmGroup(prmdict, sub_panel)
 
-    # setting resize
-    resize_panel = wx.Panel(sub_panel, wx.ID_ANY)
-    resize_layout = wx.BoxSizer(wx.VERTICAL)
-    resize_Panels = getPrmPanels(db_dict, panel, resize_panel, resize_layout)
-
-    # setting prm
-    prm_panel = wx.Panel(sub_panel, wx.ID_ANY)
-    prm_layout = wx.BoxSizer(wx.VERTICAL)
-    prm_Panels = getPrmPanels(prmdict, panel, prm_panel, prm_layout)
-
+    sub_layout.Add(prm_Controls.panel)
+    sub_layout.Add(resize_Controls.panel)
 
     # setting execute
-    exec_panel = wx.Panel(resize_panel, wx.ID_ANY)
+    exec_panel = wx.Panel(resize_Controls.panel, wx.ID_ANY)
     exec_layout = wx.BoxSizer(wx.HORIZONTAL)
-    
     element_array = (
         "createNewDir",
         "createDataBase", 
         "createMean", 
         "trainModel",
         "all")
-
     listbox = wx.ListBox(exec_panel, wx.ID_ANY, choices=element_array, style=wx.LB_SINGLE)  
     exec_text = wx.StaticText(exec_panel, wx.ID_ANY, u"set ExecuteMode")
-    
     exec_layout.Add(exec_text, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
     exec_layout.Add(listbox, flag = wx.EXPAND | wx.ALL, border = set_dir_border)
     exec_panel.SetSizer(exec_layout)
-    
-    resize_layout.Add(exec_panel, flag = wx.EXPAND | wx.ALL, border = panel_border)
-    resize_panel.SetSizer(resize_layout)
+    resize_Controls.layout.Add(exec_panel, flag = wx.EXPAND | wx.ALL, border = panel_border)
+    resize_Controls.refresh()
 
-    #layout
-    layout.Add(home_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
-    layout.Add(image_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
-    layout.Add(trainlist_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
-    layout.Add(testlist_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
-    layout.Add(dirPath_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
-    layout.Add(caffe_Panels[0], flag = wx.EXPAND | wx.ALL, border = panel_border)
-    sub_layout.Add(prm_panel, flag = wx.EXPAND | wx.ALL, border = panel_border)
-    sub_layout.Add(resize_panel, flag = wx.EXPAND | wx.ALL, border = panel_border)
-
+    # layout
     sub_panel.SetSizer(sub_layout)
     layout.Add(sub_panel, flag = wx.EXPAND | wx.ALL, border = panel_border)
     layout.Add(button_1, flag = wx.EXPAND | wx.ALL, border = panel_border)
-
     panel.SetSizer(layout)
+    
     frame.Show()
     application.MainLoop()
